725fbc6f272fc1fe84e2d2c7ec99a54a48652f8b
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-09-06 16:50:13 +0200

    Add carousel code from other project

--- Sources/CITCardsCarousel/CITCardsCarouselView.swift
+++ Sources/CITCardsCarousel/CITCardsCarouselView.swift
@@ -27,10 +27,101 @@
 import SwiftUI
 
 public struct CITCardsCarouselView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var currentIndex = 0
+    
+    private let tutorialItems = TutorialItemData.all
+    
+    private var isOnFirstPage: Bool {
+        currentIndex == 0
+    }
+    
+    private var isOnLastPage: Bool {
+        currentIndex + 1 == tutorialItems.count
+    }
+    
+    private var leftButtonImage: UIImage {
+        isOnFirstPage ? CITAsset.Images.icClose.image : CITAsset.Images.icBack.image
+    }
+    
+    private var rightButtonImage: UIImage? {
+        isOnLastPage ? nil : CITAsset.Images.icNext.image
+    }
+    
+    private var rightButtonText: String? {
+        isOnLastPage ? CITLocalizable.tutorialFinishButton : nil
+    }
+    
     public init() {}
     
     public var body: some View {
-        Text("Hello, World!")
+        VStack {
+            PaginationView(axis: .horizontal, showsIndicators: false) {
+                ForEach(Array(tutorialItems.enumerated()), id: \.offset) { index, tutorialCardItem in
+                    TutorialCard(tutorialCardItem: tutorialCardItem, showLogo: index == 0)
+                        .padding(.horizontal, 24)
+                }
+            }
+            .currentPageIndex($currentIndex.animation())
+            .padding(.bottom, UIDevice.deviceSize == .small ? CGFloat(24) : CGFloat(32))
+            
+            HStack {
+                Button(action: tappedLeftButton, label: {
+                    Image(uiImage: leftButtonImage)
+                })
+                .buttonStyle(palette: .secondary, layout: .standard)
+                .frame(width: Size.controlHeight)
+                .cornerRadius(CornerRadius.medium)
+                .transition(.opacity)
+                
+                if !isOnLastPage {
+                    CustomPageControl(currentPage: currentIndex, numberOfPages: tutorialItems.count)
+                        .transition(.scale)
+                }
+                
+                Button(action: tappedRightButton, label: {
+                    if let image = rightButtonImage {
+                        Image(uiImage: image)
+                            .renderingMode(.template)
+                    }
+                    if let text = rightButtonText {
+                        Text(text)
+                            .font(.buttonTextLarge)
+                    }
+                })
+                .buttonStyle(palette: .primary, layout: .standard)
+                .frame(maxWidth: isOnLastPage ? .greatestFiniteMagnitude : Size.controlHeight)
+                .cornerRadius(CornerRadius.medium)
+                .animation(.default)
+            }
+            .padding([.bottom, .horizontal], 24)
+        }
+        .padding(.top, 24)
+        .background(Color.backgroundPrimary.ignoresSafeArea())
+    }
+    
+    private func tappedLeftButton() {
+        if isOnFirstPage {
+            dismiss()
+        } else {
+            withAnimation {
+                currentIndex -= 1
+            }
+        }
+    }
+    
+    private func tappedRightButton() {
+        if isOnLastPage {
+            dismiss()
+        } else {
+            withAnimation {
+                currentIndex += 1
+            }
+        }
+    }
+    
+    private func dismiss() {
+        presentationMode.wrappedValue.dismiss()
     }
 }
 
