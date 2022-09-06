77c30ccee39797cfe9e4a8cef8474c33209d8eed
Lex Brouwers <lexbrouwers@coffeeit.nl>
2022-09-06 15:30:29 +0200

    Also remove test target from package

--- Package.swift
+++ Package.swift
@@ -21,8 +21,5 @@ let package = Package(
         .target(
             name: "CITCardsCarousel",
             dependencies: []),
-        .testTarget(
-            name: "CITCardsCarouselTests",
-            dependencies: ["CITCardsCarousel"]),
     ]
 )
